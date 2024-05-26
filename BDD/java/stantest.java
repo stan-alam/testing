13f36b639916859519e45196f9ef144595a73b55
stanislaw alam <stan.alam@gmail.com>
2024-05-26 10:59:03 -0400

    added notes -> performance testing linkedin

---
+++ BDD/java/stantest.java
@@ -0,0 +1,13 @@
+
+// 1. Write a program to reverse a string using StringBuffer.
+import java.io.*;
+import java.util.*;
+
+public class stantest {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        StringBuffer sb = new StringBuffer(s);
+        System.out.println(sb.reverse());
+    }
+}
+
